167ff8e1e6dbf92d2a7f7fa19dfd78464f9afbc5
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-05-09 07:58:19 -0700

    Latest commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [isStartActive, setIsStatActive] = useState(false)
+  const [isStopActive, setIsStopActive] = useState(false)
+  const [isResetActive, setIsResetActive] = useState(true)
+
+  const [isDeenergize, setIsDeenergize] = useState(false)
+  const [isStart, setStart] = useState(false)
+
+
+
+  const handleStart = () => {
+    setIsResetActive(false)
+    setIsStatActive(true)
+    setIsStopActive(false)
+    alert(1)
+  }
+
+
+  const handleStop = () => {
+    setIsResetActive(false)
+    setIsStatActive(false)
+    setIsStopActive(true)
+    alert(0)
+  }
+
+
+  const handleReset = () =>{
+    setIsResetActive(true)
+    setIsStatActive(false)
+    setIsStopActive(false)
+  }
+
+
+  const handleDeenergize = () =>{
+    setIsDeenergize(!isDeenergize)
+  }
+
+
+  const handleDeStart = () =>{
+    setStart(!isStart)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+        <div class="firstbutton">
+            <div class="one">
+              <button onClick={handleDeenergize}>Deenergize</button>
+              <input type="checkbox" name="documents" id="" checked={isDeenergize === false ? false : true}/>
+            </div>
+            <div class="two">
+              <button onClick={handleDeStart}>Start</button>
+              <input type="checkbox" name="hobbies" id="" checked={isStart === false ? false : true}/>
+            </div>
+        </div>
+
+
+            <div class="all button">
+
+              <div class="buttonA">
+                <button onClick={handleStart}>Start</button>
+              </div>
+
+
+              <div class="buttonB">
+                <button onClick={handleStop}>Stop</button>
+              </div>
+
+
+              <div class="buttonC">
+                <button onClick={handleReset}>Reset</button>
+              </div>
+
+
+              {isStartActive && <button className='bgBtn startActive'></button>}
+              {isStopActive && <button className='bgBtn stopActive'></button>}
+              {isResetActive && <button className='bgBtn resetActive'></button>}
+            </div>
+
+
     </div>
+
   );
 }
 
